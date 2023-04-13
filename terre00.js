fff3628a77c82c6acd71fad539c7281f16757017
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-13 11:20:10 -0400

    exo00 solve

---
+++ terre00.js
@@ -0,0 +1,35 @@
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "S",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var result = "";
+for (i = 0; i < alphabet.length; i++) {
+  result += alphabet[i].toLowerCase();
+}
+
+console.log(result);
