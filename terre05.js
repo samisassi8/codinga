c4720c82b4bb3475d200e11fc4f0521e9272f090
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-14 15:13:16 -0400

    terre05 solve without comment

--- terre05.js
+++ terre05.js
@@ -2,9 +2,9 @@ if (process.argv.length < 3) {
   console.log("Veuillez fournir une chaîne de caractères en argument.");
 } else {
   const str = process.argv.slice(2).join(" ");
-  console.log("str:", str);
+  //   console.log("str:", str);
   const words = str.split(" ");
-  console.log("words:", words);
+  //   console.log("words:", words);
   const reversedWords = words.map((word) => {
     let reversedWord = "";
     for (let i = word.length - 1; i >= 0; i--) {
---
+++ terre06.js
@@ -0,0 +1,2 @@
+var expression = process.argv.slice(2);
+console.log(expression);
