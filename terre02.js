3f8284189b80f3d5800d05e51cd1d606e94806b1
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-14 09:27:55 -0400

    terre02 correction

--- terre02.js
+++ terre02.js
@@ -1,2 +1,7 @@
-var message = process.argv.slice(2).join(" "); //avec process.argv on recupere un tableau de ce qui a ete ecrit sur le terminal. le .slice(2) va enlever les deux premiers elements du tableau et La méthode join() est une méthode de tableau qui permet de concaténer tous les éléments d'un tableau en une seule chaîne de caractères. Elle prend en argument un séparateur optionnel qui est utilisé pour séparer les éléments concaténés.
-console.log(message);
+var message = process.argv.slice(2); //avec process.argv on recupere un tableau de ce qui a ete ecrit sur le terminal. le .slice(2) va enlever les deux premiers elements du tableau et La méthode join() est une méthode de tableau qui permet de concaténer tous les éléments d'un tableau en une seule chaîne de caractères. Elle prend en argument un séparateur optionnel qui est utilisé pour séparer les éléments concaténés.
+
+for (word of message) {
+  console.log(word);
+}
+
+// console.log(message);
---
+++ terre03.js
@@ -0,0 +1,7 @@
+var message = process.argv[2];
+console.log(message);
+
+if (message !== undefined) {
+  var alphabet = "";
+  alphabet.fromCharCode(message.charCodeAt(), ...)
+}
