a82a7c2a8baa8044be4e4fc11f1669e05d973bf4
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-14 10:46:16 -0400

    terre03 solve

--- terre03.js
+++ terre03.js
@@ -1,7 +1,15 @@
-var message = process.argv[2];
-console.log(message);
+const startLetter = process.argv[2];
 
-if (message !== undefined) {
-  var alphabet = "";
-  alphabet.fromCharCode(message.charCodeAt(), ...)
+if (startLetter === startLetter.toUpperCase()) {
+  // La lettre donnée en argument est en majuscule
+  for (let i = startLetter.charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
+    process.stdout.write(String.fromCharCode(i).toUpperCase());
+  }
+} else {
+  // La lettre donnée en argument est en minuscule
+  for (let i = startLetter.charCodeAt(0); i <= "z".charCodeAt(0); i++) {
+    process.stdout.write(String.fromCharCode(i).toLowerCase());
+  }
 }
+
+process.stdout.write("\n");
