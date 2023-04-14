8051e1e506797a44cf4e4e8600562c94382efdeb
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-14 09:28:32 -0400

    terre02 correction

--- terre02.js
+++ terre02.js
@@ -1,4 +1,4 @@
-var message = process.argv.slice(2); //avec process.argv on recupere un tableau de ce qui a ete ecrit sur le terminal. le .slice(2) va enlever les deux premiers elements du tableau et La méthode join() est une méthode de tableau qui permet de concaténer tous les éléments d'un tableau en une seule chaîne de caractères. Elle prend en argument un séparateur optionnel qui est utilisé pour séparer les éléments concaténés.
+var message = process.argv.slice(2); //avec process.argv on recupere un tableau de ce qui a ete ecrit sur le terminal. le .slice(2) va enlever les deux premiers elements du tableau
 
 for (word of message) {
   console.log(word);
