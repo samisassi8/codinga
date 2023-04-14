03b65e84be1d8a3373ca3c43113478c7e0214cac
sami brahim <88890581+samisassi8@users.noreply.github.com>
2023-04-14 15:10:39 -0400

    terre05 solve

---
+++ terre05.js
@@ -0,0 +1,17 @@
+if (process.argv.length < 3) {
+  console.log("Veuillez fournir une chaîne de caractères en argument.");
+} else {
+  const str = process.argv.slice(2).join(" ");
+  console.log("str:", str);
+  const words = str.split(" ");
+  console.log("words:", words);
+  const reversedWords = words.map((word) => {
+    let reversedWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    }
+    return reversedWord;
+  });
+  const reversedStr = reversedWords.reverse().join(" ");
+  console.log(reversedStr);
+}
